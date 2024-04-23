2db5b83e066f7b3fadab62c2f52ffc4a569a8c0b
Thalisson Braga <thalisson.braga1028@gmail.com>
2024-04-23 12:51:08 -0300

    Challenge resolved

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
+    public class Nokia : Smartphone
+    {   
+        public Nokia(string numero, string imei, string modelo, int memoria) : base(numero, imei, modelo, memoria){
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"INSTALANDO APLICATIVO NOKIA: {nomeApp}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Celular Nokia");
+
+Nokia c1 = new Nokia("21999999", "09092M", "LUMIA420", 8);
+c1.Ligar();
+c1.InstalarAplicativo("Subway Surfers\n");
+
+System.Console.WriteLine("Celular Iphone");
+Iphone c2 = new Iphone("2131424242", "23827G", "IPHONE 20", 120);
+c2.ReceberLigacao();
+c2.InstalarAplicativo("Flappy Bird\n");
